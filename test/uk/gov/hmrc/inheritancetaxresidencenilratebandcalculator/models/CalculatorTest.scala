a9a239c58f20ab107825f65afe17837ee1f1eb14
Nick Wilson <thenickwilson@gmail.com>
2016-11-21 14:45:52 +0000

    RBRNC-42 WIP on the basics of RNRB calculation

---
+++ test/uk/gov/hmrc/inheritancetaxresidencenilratebandcalculator/models/CalculatorTest.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.inheritancetaxresidencenilratebandcalculator.models
+
+import org.joda.time.LocalDate
+import uk.gov.hmrc.play.test.UnitSpec
+
+class CalculatorTest extends UnitSpec {
+
+  "Calculator" when {
+
+    "all assets are left to direct descendents and leaving a property worth more than the RNRB threshold" must {
+
+      "give RNRA equal to the threshold for the 2020/21 tax year" in {
+        Calculator(new LocalDate(2021, 1, 1), 490000, 300000) shouldBe CalculationResult(175000, 0)
+      }
+
+      "give RNRA equal to the threshold for the 2019/20 tax year" in {
+        Calculator(new LocalDate(2020, 1, 1), 490000, 300000) shouldBe CalculationResult(150000, 0)
+      }
+    }
+
+    "all assets are left to direct descendents and leaving a property worth less than the RNRB threshold" must {
+
+      "give RNRA equal to the property value and CFA equal to (threshold - property value)" in {
+        Calculator(new LocalDate(2020, 1, 1), 470000, 80000) shouldBe CalculationResult(80000, 70000)
+      }
+    }
+  }
+}
