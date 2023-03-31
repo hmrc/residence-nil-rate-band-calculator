463b4c0d5f820b4beffdf3b9f87b444740d0a4b8
JoshMiernik <josh.miernik@digital.hmrc.gov.uk>
2023-03-31 15:16:50 +0100

    DL-10005 Java11 and dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,14 @@ import play.core.PlayVersion
 
 private object AppDependencies {
 
+  val bootstrapVersion = "7.15.0"
+  val mockitoCoreVersion = "5.2.0"
+
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"         %% "bootstrap-backend-play-28"   % "5.25.0",
+    "uk.gov.hmrc"         %% "bootstrap-backend-play-28"   % bootstrapVersion,
     "org.scalaj"          %% "scalaj-http"                 % "2.4.2",
-    "com.typesafe.play"   %% "play-json-joda"              % "2.9.2",
+    "com.typesafe.play"   %% "play-json-joda"              % "2.9.4",
   )
 
   trait TestDependencies {
@@ -20,12 +23,11 @@ private object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc"               %% "bootstrap-test-play-28" % "5.24.0",
-        "org.pegdown"               %  "pegdown"            % "1.6.0",
-        "com.typesafe.play"         %% "play-test"          % PlayVersion.current,
-        "org.scala-tools.testing"   %  "test-interface"     % "0.5",
-        "org.mockito"               %  "mockito-core"       % "3.12.4",
-        "org.scalatestplus"         %%  "scalatestplus-mockito"     % "1.0.0-M2",
+        "uk.gov.hmrc"               %% "bootstrap-test-play-28" % bootstrapVersion,
+        "com.typesafe.play"         %% "play-test"              % PlayVersion.current,
+        "org.scala-tools.testing"   %  "test-interface"         % "0.5",
+        "org.mockito"               %  "mockito-core"           % mockitoCoreVersion,
+        "org.scalatestplus"         %%  "scalatestplus-mockito" % "1.0.0-M2",
       ).map(_ % scope)
     }.test
   }
@@ -36,10 +38,9 @@ private object AppDependencies {
       override lazy val scope: String = "it"
 
       override lazy val test = Seq(
-        "org.pegdown"               % "pegdown"               % "1.6.0"                 % scope,
-        "com.typesafe.play"         %% "play-test"            % PlayVersion.current     % scope,
-        "uk.gov.hmrc"               %% "bootstrap-test-play-28" % "5.24.0"            % scope,
-        "org.mockito"               % "mockito-core"          % "3.12.4"                 % scope
+        "com.typesafe.play"         %% "play-test"              % PlayVersion.current     % scope,
+        "uk.gov.hmrc"               %% "bootstrap-test-play-28" % bootstrapVersion        % scope,
+        "org.mockito"               % "mockito-core"            % mockitoCoreVersion      % scope
       )
     }.test
   }
