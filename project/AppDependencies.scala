69b910224fb12624611ab7d24c2332b0394221a2
Eoghan Spillane <201935567+Eoghan-Spillane-Accenture@users.noreply.github.com>
2025-08-26 16:39:39 +0100

    DL-17202: Update bootstrap to fix bobby violation. (#94)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import play.core.PlayVersion
 
 private object AppDependencies {
 
-  val bootstrapVersion   = "9.13.0"
+  val bootstrapVersion   = "10.1.0"
   val mockitoCoreVersion = "5.2.0"
 
   val compile: Seq[ModuleID] = Seq(
